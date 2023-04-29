ab51b7cc18bd003842547c9a5e1e7b1b783e4b18
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2023-04-29 18:48:48 -0300

    feat: implementa rest api para estatísticas
    Implementa requests que retorno body JSON com estatísticas com o número de pacientes, consultas e exames

--- src/main/java/com/labmedicine/labmedical/api/EstatisticasController.java
+++ src/main/java/com/labmedicine/labmedical/api/EstatisticasController.java
@@ -5,6 +5,8 @@ import com.labmedicine.labmedical.repositories.ConsultaRepository;
 import com.labmedicine.labmedical.repositories.ExameRepository;
 import com.labmedicine.labmedical.repositories.PacienteRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,7 +21,7 @@ public class EstatisticasController {
 
   @GetMapping
   @ResponseBody
-  public Estatisticas estatisticas() {
+  public ResponseEntity<Estatisticas> getEstatisticas() {
     Estatisticas estatisticas = new Estatisticas();
     int pacientes = pacienteRepository.findAll().size();
     int consultas = consultaRepository.findAll().size();
@@ -29,6 +31,6 @@ public class EstatisticasController {
     estatisticas.setConsultas(consultas);
     estatisticas.setExames(exames);
 
-    return estatisticas;
+    return new ResponseEntity<>(estatisticas, HttpStatus.OK);
   }
 }
