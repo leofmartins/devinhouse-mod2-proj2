9c68301ce489bb492733405a0567c5c06de54ad0
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2023-04-30 13:52:16 -0300

    fix: Retira anotação @Data das classes de entidade
    Retira anotação @Data (Lombok) das entidades a fim de corrigir o bug de stackoverflow e substitui por @Getters e @Setters apropriados

--- src/main/java/com/labmedicine/labmedical/model/Usuario.java
+++ src/main/java/com/labmedicine/labmedical/model/Usuario.java
@@ -4,25 +4,27 @@ import com.labmedicine.labmedical.model.enums.TipoEspecializacao;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Usuario extends Pessoa {
 
   @NotNull
+  @Getter
+  @Setter
   private String cadastroCRM;
 
   @NotNull
+  @Getter
+  @Setter
   private TipoEspecializacao especializacao;
 
   @NotNull
   @Size(min = 8)
+  @Getter
+  @Setter
   private String senha;
 }
