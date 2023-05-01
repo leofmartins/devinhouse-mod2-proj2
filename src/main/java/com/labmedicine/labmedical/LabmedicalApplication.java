818e7d18bbbd86ec40954176e40d55a4dac4f0dd
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2023-04-30 23:57:02 -0300

    feat: script para carregar dados iniciais

--- src/main/java/com/labmedicine/labmedical/LabmedicalApplication.java
+++ src/main/java/com/labmedicine/labmedical/LabmedicalApplication.java
@@ -1,7 +1,17 @@
 package com.labmedicine.labmedical;
 
+import com.labmedicine.labmedical.api.UsuarioController;
+import com.labmedicine.labmedical.model.Usuario;
+import com.labmedicine.labmedical.model.enums.TipoEspecializacao;
+import com.labmedicine.labmedical.model.enums.TipoEstadoCivil;
+import com.labmedicine.labmedical.model.enums.TipoGenero;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Calendar;
+import java.util.Date;
 
 @SpringBootApplication
 public class LabmedicalApplication {
